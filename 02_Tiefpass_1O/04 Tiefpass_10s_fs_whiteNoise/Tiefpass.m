883663604bb6ac419e72ef1dc920945c3bbe43ed
maknab <m.knabbe@gmx.de>
2016-04-21 15:54:51 +0200

    correcting filter type part 1
    changing 'Hochpass' to 'Tiefpass'

--- 02_Tiefpass_1O/04 Tiefpass_10s_fs_whiteNoise/Tiefpass.m
+++ 02_Tiefpass_1O/04 Tiefpass_10s_fs_whiteNoise/Tiefpass.m
@@ -1,7 +1,3 @@
-
-% load trained net
-load HP_net.mat;
-
 % view signals
 figure(1)
 plot(input_signal);
@@ -9,18 +5,22 @@ hold on;
 plot(output_signal);
 title('Input- and Output- Signals fed to the net');
 
-% prepare signals
+% prepare signals and net
 X = con2seq(input_signal');
 T = con2seq(output_signal');
+lrn_net = layrecnet(1:2, 8);
+lrn_net.trainParam.show = 5;
+lrn_net.trainParam.epochs = 250;
 [Xs,Xi,Ai,Ts] = preparets(lrn_net,X,T);
 
-% fed input signal to net
-Y = lrn_net(Xs,Xi,Ai);
+% train net
+lrn_net = train(lrn_net,Xs,Ts,Xi,Ai);
+
+% save net
+save HP_net.mat lrn_net;
 
 % view net output
+Y = lrn_net(Xs,Xi,Ai);
 figure(2)
 plot(cell2mat(Y));
 title('Output-Signal from the net');
-
-figure(3)
-plotresponse(Ts,Y)
